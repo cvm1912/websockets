3fba1178b6cf9855333fd7a1d5b444f9bc401cf7
cvm1912 <rajshivam1912@gmail.com>
2026-01-15 15:45:57 +0530

    broadcasting

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import WebSocket, {WebSocketServer} from "ws";
 const wss = new WebSocketServer({port:8080})
 
 let usercount = 0;
-
+let allSockets = [];
 wss.on("connection",function(socket){
+    // when ever client get connect
+    allSockets.push(socket);
+
    usercount = usercount+1;
    console.log("user connected",usercount);
 
    socket.on("message",(event)=>{
-        socket.send("thanks for the message git a"+ event.toString());
+        allSockets.forEach((message)=>{
+            if(message != socket){
+                message.send(event.toString());
+            }
+        })
    })
 })
 
