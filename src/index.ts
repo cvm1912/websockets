23cf5d46be931cc8fc0adc64286ad8405c7370be
cvm1912 <rajshivam1912@gmail.com>
2026-01-15 15:25:03 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ let usercount = 0;
 wss.on("connection",function(socket){
    usercount = usercount+1;
    console.log("user connected",usercount);
+
+   socket.on("message",(event)=>{
+        socket.send("thanks for the message git a"+ event.toString());
+   })
 })
 
 
