536eab35afa983c59df9a5bbc90105ba82860653
cvm1912 <rajshivam1912@gmail.com>
2026-01-15 14:52:47 +0530

    ping-pong added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 
 import WebSocket, {WebSocketServer} from "ws";
-
-// it is same like const app = express()
 const wss = new WebSocketServer({port:8080})
 
 wss.on("connection",function(socket){
   console.log("user connected")
-  setInterval(()=>{
-    socket.send("Hello, WebSockets Users !!")
-  },10000);
+
+  socket.on("message", (e)=>{
+       if(e.toString() === "ping"){
+        socket.send("pong")
+       }
+  })
 })
+
+
+
