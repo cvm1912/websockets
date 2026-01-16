015c5d97e342f0241f68ea749cdf2e1e67df6f1b
cvm1912 <rajshivam1912@gmail.com>
2026-01-16 11:07:49 +0530

    broadcasting a message added

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import {WebSocketServer} from 'ws'
 const ws = new WebSocketServer({port:8080});
 
 let userCount = 0;
+let allSockets = [];
 
 // new connection 
 ws.on('connection',(socket)=>{
+  allSockets.push(socket);
   userCount++;
   socket.send(`total connections ${userCount}`);
   socket.send("client connected");
-  socket.on("message",(event)=>{
-    socket.send("hello Dear",)
+  socket.on("message",(message)=>{
+    allSockets.forEach((sock)=>sock.send("Hello, Mr."+ message.toString()))
   })
 })
 
