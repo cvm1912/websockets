927bdd3212c1941b593934ae23e20d9d24211b5b
cvm1912 <rajshivam1912@gmail.com>
2026-01-16 10:52:17 +0530

    server-response

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,14 @@ ws.on('connection',(socket)=>{
   userCount++;
   socket.send(`total connections ${userCount}`);
   socket.send("client connected");
+  socket.on("message",(event)=>{
+    socket.send("hello Dear",)
+  })
 })
+
+
+
+// how do you recive and send a message to server 
 
 
 
